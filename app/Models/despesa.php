34a5e794a4d7a0b08032bbf112e610234ddc8b64
vitoreal <vitoreselecao@gmail.com>
2024-04-19 09:49:42 -0300

    incluindo campos no modelo de despesa

--- app/Models/despesa.php
+++ app/Models/despesa.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class despesa extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'despesa';
+
+    protected $fillable = [
+        'nome',
+        'valor',
+        'data_despesa',
+        'id_moeda',
+        'id_paises',
+        'id_viagem',
+        'id_categoria',
+        'id_categoria_personalizada',
+        'id_metodo_pagamento',
+        'outros_metodo_pagamento'
+    ];
+
 }
